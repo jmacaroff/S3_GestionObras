0e833615d47a16a07e5e02a59008028a45574ac3
Joaquin Antonio Macaroff Perez <jomax1398@gmail.com>
2020-09-02 13:53:25 -0300

    Initial Commit

---
+++ GestionObras/Controllers/DepositoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionObras.Controllers
+{
+    public class DepositoController : Controller
+    {
+        //private readonly ApplicationDbContext _db;
+        //[BindProperty]
+        //public Material Material { get; set; }
+        public IActionResult Inventario()
+        {
+            return View();
+        }
+        public IActionResult Upsert(int? id)
+        {
+            //inicializar material
+            if (id == null)
+            {
+                //create
+                //return View(Material); //se debe insertar el objeto
+               
+            }
+            //if (Material == null)
+            //{
+            //    return NotFound();
+            //}
+            return View();//se debe insertar el objeto material
+        }
+
+
+        #region API Calls
+        #endregion
+    }
+}
